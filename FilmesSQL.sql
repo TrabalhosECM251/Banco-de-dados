7766a8c0d4f3ddb7b753e7da0eac3ed1a2a31d41
Carolina-CG <79457723+Carolina-CG@users.noreply.github.com>
2021-10-20 16:14:09 -0300

    Add files via upload
    Filmes, Usuário e Review

---
+++ FilmesSQL.sql
@@ -0,0 +1,344 @@
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Um Porto Seguro', 'Romance', 'GloboPlay', 10);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Querido John', 'Romance', 'Netflix', 9);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Diário de uma paixão', 'Romance', 'GooglePlay', 9.5);
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Amizade Colorida', 'Comedia Romantica', 'GooglePlay', 8);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Qual o seu número', 'Comedia Romantica', 'PrimeVideo, Star+', 10);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('A verdade nua e crua', 'Comedia Romantica', 'Star+', 10);
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('American Pie', 'Comedia', 'Netflix', 8);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Os Estagiários', 'Comedia', 'Netflix', 9);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('As Branuelas', 'Comedia', 'Netflix', 10);
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Procurando Nemo', 'Infaltil', 'Disney+', 8);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Os Incriveis', 'Infaltil', 'Disney+', 9.5);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Monstros S.A.', 'Infaltil', 'Disney+', 10);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Carros', 'Infaltil', 'Disney+', 8);
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Herbie - Meu Fusca Turbinado', 'Adolecente', 'Disney+', 10);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Sexta-feira muito louca', 'Adolecente', 'Disney+', 9.5);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('17 outra vez', 'Adolecente', 'GooglePlay', 9);
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Velozes e furiosos', 'Ação', 'Telecine', 10);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('O Preço do amanhã', 'Ação', 'Netflix', 9);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Transformers', 'Ação', 'PrimeVideo', 8);
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Jumanji', 'Aventura', 'Netflix', 9);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Viagem ao cento da terra', 'Aventura', 'YouTube', 9);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('A Lenda do Tesouro Perdido', 'Aventura', 'Disney+', 9);
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Avatar', 'Ficção Científica', 'Disney+', 9.5);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Jurassic World', 'Ficção Científica', 'Netflix', 8);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('O Doador de memorias', 'Ficção Científica', 'PrimeVideo', 7.5);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Os passageiros', 'Ficção Científica', 'Netflix', 8);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('A Hospedeira', 'Ficção Científica', 'PrimeVideo', 9);
+
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Agora é para sempre', 'Drama', 'YouTube', 8.5);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Extraordinário', 'Drama', 'Telecine', 9);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('O menino que descobriu o vento', 'Drama', 'Netflix', 9);
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('O garoto da casa ao lado', 'Suspense', 'Netflix', 7.5);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Nerve', 'Suspense', 'Netflix', 9);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Um pequeno favor', 'Suspense', 'Telecine', 6.5);
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Garota Infernal', 'Terror', 'Star+', 7);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('A garota da capa vermelha', 'Terror', 'YouTube', 7);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Um lugar silencioso', 'Terror', 'GooglePlay', 9);
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Guerra Mundial Z', 'Apocalipse', 'PrimeVideo', 9);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('2012', 'Apocalipse', 'Netflix', 9);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Terremoto - A falha de San Andreas', 'Apocalipse', 'Netflix', 10);
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Estrelas alem do tempo', 'Baseado Hist Real', 'Telecine', 10);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Intocáveis', 'Baseado Hist Real', 'Telecine', 9);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Prenda-me se for capaz', 'Baseado Hist Real', 'Netflix', 6.5);
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Jogada Certa', 'Esporte', 'Star+', 8.5);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Golpe Baixo', 'Esporte', 'Netflix', 9);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Sonhos no Gelo', 'Esporte', 'Disney+', 9);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Um sonho possivel', 'Esporte', 'Telecine', 9);
+
+
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Little fires everywhere', 'Serie', 'PrimeVideo', 10);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Outlander', 'Serie', 'Netflix', 10);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Younger', 'Serie', 'PrimeVideo', 10);
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Good morning call', 'Dorama', 'Netflix', 7);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Love Alarme', 'Dorama', 'Netflix', 8.5);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Pousada do amor', 'Dorama', 'Netflix', 10);
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('A Silent Voice', 'Anime', 'Netflix', 8);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Your Name', 'Anime', 'YouTube', 9);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Death Note ', 'Anime', 'Netflix', 7);
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('High school musical', 'Musical', 'Disney+', 7);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Mamma Mia!', 'Musical', 'Star+', 7.5);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('O rei do show', 'Musical', 'Disney+', 7);
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Homem Aranha', 'Super Herói', 'Netflix', 9);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Homem de Ferro', 'Super Herói', 'Disney+', 10);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Mulher Maravilha', 'Super Herói', 'Netflix', 9.5);
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Se eu fosse você', 'Brasileiros', 'Now', 8);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Minha mãe é uma peça', 'Brasileiros', 'GloboPlay', 9);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('S.O.S mulheres ao mar', 'Brasileiros', 'YouTube', 9);
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Maze Runner', 'Trilogias', 'Star+', 8);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('50 tons de cinza', 'Trilogias', 'Netflix', 9);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Jogos Vorazes', 'Trilogias', 'Netflix', 8);
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Harry Potter', 'Fantasia', 'Now', 10);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Os Seis Signos da Luz', 'Fantasia', 'YouTube', 6.5);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Sou o numero 4', 'Fantasia', 'Now', 7);
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Framing Britney Spears: A Vida de uma Estrela', 'Documentários', 'GloboPlay', 7);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Cowspiracy - O segredo da sustentabilidade', 'Documentários', 'Netflix', 6);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Cosmos', 'Documentários', 'Netflix', 7.5);
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('A.I. - Inteligência Artificial', 'AI', 'Telecine', 9.5);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('O homem bicentenário', 'AI', 'Star+', 10);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Archive', 'AI', 'Telecine', 6.5);
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Um  homem de sorte', 'Guerra', 'PrimeVideo', 9);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Até o ultimo homem', 'Guerra', 'PrimeVideo', 10);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Por amor e honra', 'Guerra', 'PrimeVideo', 8);
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('De volta para o futuro', 'Futurista', 'Netflix', 9);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Elysium', 'Futurista', 'PrimeVideo', 7.5);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Minority Report - A Nova Lei', 'Futurista', 'GloboPlay', 7);
+
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Sempre ao seu lado', 'Cachorro', 'GloboPlay', 10);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Meu amigo Enzo', 'Cachorro', 'Now', 9);
+INSERT INTO Filmes 
+(nome, tema, disponivel, nota)
+VALUES ('Quatro vidas de um cachorro', 'Cachorro', 'YouTube', 9);
+
+
+
+INSERT INTO Usuario 
+(nome, email, senha)
+VALUES ('Carolina Gutierrez', 'caaroolinag@gmail.com', 'C@!0l7#25');
+INSERT INTO Usuario 
+(nome, email, senha)
+VALUES ('Marilise S. C. Gutierrez', 'marilisegutierrez50@gmail.com', 'M!*ao08$!8');
+INSERT INTO Usuario 
+(nome, email, senha)
+VALUES ('Agostinho B. M. Gutierrez', 'agostinhogutierrez@gmail.com', '@&87st93');
+INSERT INTO Usuario 
+(nome, email, senha)
+VALUES ('Alexandre Luiz Alves', 'alexandrealvees@gmail.com', '%le177$4');
+INSERT INTO Usuario 
+(nome, email, senha)
+VALUES ('Felipe Gutierrez', 'felipegutierrez@gmail.com', '%e!@27');
+INSERT INTO Usuario 
+(nome, email, senha)
+VALUES ('Alessandra Paixão Castilho', 'alessandrapaixãocastilho@gmail.com', 'GT!#35!');
+INSERT INTO Usuario 
+(nome, email, senha)
+VALUES ('Maiara Baptista', 'map151012@gmail.com', 'M23$59*!');
+
+
+
+INSERT INTO Review 
+(idFilmes, idUsuario, Classifica, NotaUsuario, Comentario)
+VALUES (1, 2,'Bom', 8, 'Gostei muito do filme. Recomendo');
+INSERT INTO Review 
+(idFilmes, idUsuario, Classifica, NotaUsuario, Comentario)
+VALUES (5, 3,'Medio', 6, 'Chatinho');
+INSERT INTO Review 
+(idFilmes, idUsuario, Classifica, NotaUsuario, Comentario)
+VALUES (6, 7,'Ruim', 4,'Não gostei');
+INSERT INTO Review 
+(idFilmes, idUsuario, Classifica, NotaUsuario, Comentario)
+VALUES (1, 5,'Chato', 3, 'Parado, com cenas fracas');
+INSERT INTO Review 
+(idFilmes, idUsuario, Classifica, NotaUsuario, Comentario)
+VALUES (16, 4,'Maravilhoso', 10, 'Muito muito bom, mega recomendo');
+INSERT INTO Review 
+(idFilmes, idUsuario, Classifica, NotaUsuario, Comentario)
+VALUES (81, 1,'Muito bom', 9, 'Gostei muito do filme.');
+INSERT INTO Review 
+(idFilmes, idUsuario, Classifica, NotaUsuario, Comentario)
+VALUES (50, 6,'Legal', 7.5, 'Recomendo');
+INSERT INTO Review 
+(idFilmes, idUsuario, Classifica, NotaUsuario, Comentario)
+VALUES (3, 7,'Gostei', 8.6, 'Gostei muito do filme. Recomendo');
+INSERT INTO Review 
+(idFilmes, idUsuario, Classifica, NotaUsuario, Comentario)
+VALUES (23, 5,'Bom', 8, 'Gostei muito.');
+INSERT INTO Review 
+(idFilmes, idUsuario, Classifica, NotaUsuario, Comentario)
+VALUES (77, 3,'Bom', 8, 'Gostei bastante');
+
+
+
